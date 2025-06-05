0b47b9b81f7c759a757ff5a742953776f9dc7acf
keliumJU <keliumju@gmail.com>
2025-06-05 15:51:23 -0500

    Remove warnings

--- src/main/java/co/edu/itp/svu/service/mapper/ArchivoAdjuntoMapper.java
+++ src/main/java/co/edu/itp/svu/service/mapper/ArchivoAdjuntoMapper.java
@@ -1,18 +1,12 @@
 package co.edu.itp.svu.service.mapper;
 
 import co.edu.itp.svu.domain.ArchivoAdjunto;
-import co.edu.itp.svu.domain.Pqrs;
-import co.edu.itp.svu.domain.Respuesta;
 import co.edu.itp.svu.service.dto.ArchivoAdjuntoDTO;
-import co.edu.itp.svu.service.dto.PqrsDTO;
-import co.edu.itp.svu.service.dto.RespuestaDTO;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ArchivoAdjuntoMapper extends EntityMapper<ArchivoAdjuntoDTO, ArchivoAdjunto> {
-    // Mapeo básico de ArchivoAdjunto a ArchivoAdjuntoDTO
     ArchivoAdjuntoDTO toDto(ArchivoAdjunto s);
 
-    // Mapeo básico de ArchivoAdjuntoDTO a ArchivoAdjunto
     ArchivoAdjunto toEntity(ArchivoAdjuntoDTO dto);
 }
