5dca1188cf3a615717b1c67fc26ec93cca3b979e
Luis Carlos <luiscarjo157@gmail.com>
2025-06-07 22:58:38 -0500

    new fields are added

--- src/main/webapp/app/shared/model/respuesta.model.ts
+++ src/main/webapp/app/shared/model/respuesta.model.ts
@@ -1,10 +1,13 @@
 import { type IPqrs } from '@/shared/model/pqrs.model';
+import { type IArchivoAdjunto } from '@/shared/model/archivo-adjunto.model';
 
 export interface IRespuesta {
   id?: string;
   contenido?: string;
   fechaRespuesta?: Date;
   pqr?: IPqrs | null;
+  archivosAdjuntosDTO?: IArchivoAdjunto[] | null;
+  _transientAttachments?: IArchivoAdjunto[];
 }
 
 export class Respuesta implements IRespuesta {
@@ -13,5 +16,7 @@ export class Respuesta implements IRespuesta {
     public contenido?: string,
     public fechaRespuesta?: Date,
     public pqr?: IPqrs | null,
+    public archivosAdjuntosDTO?: IArchivoAdjunto[] | null,
+    public _transientAttachments?: IArchivoAdjunto[],
   ) {}
 }
