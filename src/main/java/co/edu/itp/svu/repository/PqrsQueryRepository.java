f4d26e65fc03f5489995bd9f675494dca9645874
keliumJU <keliumju@gmail.com>
2025-06-27 21:42:54 -0500

    Add the custom classes to extend the PqrsRepository functionalities

---
+++ src/main/java/co/edu/itp/svu/repository/PqrsQueryRepository.java
@@ -0,0 +1,16 @@
+package co.edu.itp.svu.repository;
+
+import co.edu.itp.svu.domain.Pqrs;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PqrsQueryRepository {
+    /**
+     * Searches for Pqrs entities where the title OR fileNumber matches the query.
+     *
+     * @param query    the string to search for in title and fileNumber.
+     * @param pageable the pagination information.
+     * @return a page of matching Pqrs entities.
+     */
+    Page<Pqrs> search(String query, Pageable pageable);
+}
