0489d8b8d9772e39a68285a0efe61540cf651bfc
keliumJU <keliumju@gmail.com>
2025-07-07 14:38:29 -0500

    Handle the methods how Optional in office repository

--- src/main/java/co/edu/itp/svu/repository/OficinaRepository.java
+++ src/main/java/co/edu/itp/svu/repository/OficinaRepository.java
@@ -1,8 +1,7 @@
 package co.edu.itp.svu.repository;
 
 import co.edu.itp.svu.domain.Oficina;
-import co.edu.itp.svu.domain.Pqrs;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +10,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface OficinaRepository extends MongoRepository<Oficina, String> {
-    Oficina findByResponsable_Id(String userId); // Método para encontrar Oficina por usuario
-    Oficina findByResponsable_Login(String login); // Método para encontrar Oficina por usuario
+    Optional<Oficina> findByResponsable_Id(String userId);
+
+    Optional<Oficina> findByResponsable_Login(String login);
+
     Oficina findByNombre(String name);
 }
