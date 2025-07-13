2f579a65c5ff840e39b3daf18cb2e7027309aba3
keliumJU <keliumju@gmail.com>
2025-07-12 23:46:28 -0500

    Get the users in the response and fix the name for the frontdesk office

--- src/main/java/co/edu/itp/svu/service/mapper/OficinaMapper.java
+++ src/main/java/co/edu/itp/svu/service/mapper/OficinaMapper.java
@@ -7,5 +7,18 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity {@link Oficina} and its DTO {@link OficinaDTO}.
  */
-@Mapper(componentModel = "spring")
-public interface OficinaMapper extends EntityMapper<OficinaDTO, Oficina> {}
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
+public interface OficinaMapper extends EntityMapper<OficinaDTO, Oficina> {
+    @Override
+    @Mapping(source = "responsable", target = "responsableDTO")
+    OficinaDTO toDto(Oficina Oficina);
+
+    @Override
+    @Mapping(source = "responsableDTO", target = "responsable")
+    Oficina toEntity(OficinaDTO OficinaDTO);
+
+    @Override
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "responsableDTO", target = "responsable")
+    void partialUpdate(@MappingTarget Oficina Oficina, OficinaDTO OficinaDTO);
+}
