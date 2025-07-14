67ebc47b1c1bad1f59a75d9d5950aec1e5c5a2c5
keliumJU <keliumju@gmail.com>
2025-07-14 17:31:37 -0500

    Implement the new footers in the main layout

--- src/main/webapp/app/app.vue
+++ src/main/webapp/app/app.vue
@@ -1,24 +1,41 @@
 <template>
-  <div id="app">
+  <div id="app" class="app-wrapper">
     <ribbon></ribbon>
     <div id="app-header">
       <jhi-navbar></jhi-navbar>
     </div>
     <div class="container-fluid">
-      <div class="card jh-card">
-        <router-view></router-view>
-      </div>
-      <b-modal id="login-page" hide-footer lazy>
-        <template #modal-title>
-          <span data-cy="loginTitle" id="login-title" v-text="t$('login.title')"></span>
-        </template>
-        <login-form></login-form>
-      </b-modal>
-
-      <jhi-footer></jhi-footer>
+      <main class="main-content">
+        <div class="card jh-card">
+          <router-view></router-view>
+        </div>
+        <b-modal id="login-page" hide-footer lazy>
+          <template #modal-title>
+            <span data-cy="loginTitle" id="login-title" v-text="t$('login.title')"></span>
+          </template>
+          <login-form></login-form>
+        </b-modal>
+      </main>
+      <svu-auth-footer v-if="authenticated"></svu-auth-footer>
+      <svu-footer v-else></svu-footer>
     </div>
     <b-toaster name="b-toaster-top-right" class="p-3"></b-toaster>
   </div>
 </template>
 
 <script lang="ts" src="./app.component.ts"></script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.main-content {
+  flex-grow: 1;
+}
+</style>
