99cba3fa203c9279fbd0ab0102a4229956bf47b7
Luis Carlos <luiscarjo157@gmail.com>
2025-08-01 14:41:11 -0500

    New subject added

--- src/main/webapp/app/shared/composables/use-permissions.ts
+++ src/main/webapp/app/shared/composables/use-permissions.ts
@@ -1,7 +1,19 @@
+import sidebar from '@/entities/pqrs/sidebar';
 import { useAccountStore } from '@/shared/config/store/account-store';
 
 type Action = 'view' | 'create' | 'edit' | 'delete' | 'assign' | 'respond' | 'resolve' | 'close' | 'assign_due_date';
-type Subject = 'pqrs' | 'responses' | 'attached_files' | 'users' | 'dashboard' | 'notifications' | 'offices' | 'reports' | 'admin';
+
+type Subject =
+  | 'pqrs'
+  | 'responses'
+  | 'sidebar'
+  | 'attached_files'
+  | 'users'
+  | 'dashboard'
+  | 'notifications'
+  | 'offices'
+  | 'reports'
+  | 'admin';
 
 const permissions = {
   admin: {
@@ -11,13 +23,13 @@ const permissions = {
   },
   front_desk: {
     pqrs: ['view', 'create', 'edit'],
-    responses: ['view', 'create', 'edit'],
     notifications: ['view', 'create', 'edit'],
   },
   functionary: {
     pqrs: ['view', 'respond', 'resolve'],
     responses: ['view', 'create'],
     notifications: ['view', 'create', 'edit'],
+    sidebar: ['view'],
   },
   user: {
     pqrs: ['view'],
